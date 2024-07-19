edb70aef6235dccd2a5ad5895a4484c6ce815353
ramsuthar12 <b22ee081@iitj.ac.in>
2024-07-19 17:16:20 +0530

    connecting with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home'
 import AllTasks from './pages/AllTasks'
 import ImportantTasks from './pages/ImportantTasks'
 import CompletedTasks from './pages/CompletedTasks'
 import IncompleteTasks from './pages/IncompleteTasks'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  useEffect(()=> {
+    if (isLoggedIn === false){
+      navigate("/signup ");
+    }
+  }, [isLoggedIn, navigate]);
+  // 
   return (
     <div className='bg-gray-900 text-white h-screen p-2 relative'>
-      <Router>
         <Routes>
-          <Route axact path='/' element={<Home />}>
+          <Route exact path='/' element={<Home />}>
             <Route index element={<AllTasks />}/>
             <Route path='/importanttasks' element={<ImportantTasks />}/>
             <Route path='/completedtasks' element={<CompletedTasks />}/>
@@ -22,7 +30,6 @@ const App = () => {
           <Route path='/signup' element={<Signup />} />
           <Route path='/login' element={<Login />} />
         </Routes>
-      </Router>
     </div>
   )
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios";
 
 const Signup = () => {
+    const [Data, setData] = useState({ username: "", email: "", password: "" });
+    const history = useNavigate();
+    const change = (e)=> {
+        const { name, value } = e.target;
+        setData({ ...Data, [name]: value });
+    };
+    const submit = async()=> {
+        try {
+            if (Data.username === "" || Data.email === "" || Data.password === ""){
+                alert("All fields are mandotory.");
+            } else{
+                const response = await axios.post("http://localhost:1000/api/v1/sign-in", Data);
+                setData({ username: "", email: "", password: "" });
+                alert(response.data.message);
+                history("/login");
+            }
+        } catch (error) {
+            alert(error.response.data.message);
+        }
+    }
+
+
     return (
         <div className='h-[98vh] flex items-center justify-center'>
             <div className='p-4 w-2/6 rounded-lg bg-black h-[47%] flex flex-col justify-between'>
@@ -11,22 +34,28 @@ const Signup = () => {
                     placeholder='username'
                     className='bg-slate-700 px-3 py-2 my-3 w-full rounded'
                     name='username'
+                    value={Data.username}
+                    onChange={change}
                 />
                 <input 
                     type="email"
                     placeholder='email'
                     className='bg-slate-700 px-3 py-2 my-3 w-full rounded'
-                    name='xyz@gmail.com'
-                    required    
+                    name='email'
+                    value={Data.email}
+                    required
+                    onChange={change}    
                 />
                 <input 
                     type="password"
                     placeholder='password'
                     className='bg-slate-700 px-3 py-2 my-3 w-full rounded'
                     name='password'
+                    value={Data.password}
+                    onChange={change}
                 />
                 <div className='w-full flex justify-between items-center'>
-                    <button type="button" className="btn btn-primary my-2">Sign Up</button>
+                    <button type="button" className="btn btn-primary my-2" onClick={submit}>Sign Up</button>
                     <Link to="/login" className='text-sm hover:text-blue-500 '>Already a User? Then please Login</Link>
                 </div>
             </div>
